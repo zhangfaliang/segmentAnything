d9dd0cd00cb5b63faa9496a3f0784bc77ec84a56
zhangfaliang <zhangfaliang@doublefs.com>
2023-05-19 11:56:54 +0800

    test

--- demo/start.js
+++ demo/start.js
@@ -7,22 +7,22 @@ const app = express();
 app.use(express.static(path.join(__dirname, "dist")));
 //添加 connect-history-api-fallback 中间件
 
-// app.use(
-//   history({
-//     disableDotRule: true,
-//     // verbose: true,
-//     index: "/",
-//   })
-// );
+app.use(
+  history({
+    disableDotRule: true,
+    verbose: true,
+    index: "/",
+  })
+);
 
 app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
 // 任何未处理的请求都会被发送到 index.html
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "dist", "index.html"));
-// });
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
 
 app.listen(8080);
 console.log("listen:8080");
