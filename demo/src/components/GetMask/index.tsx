bfd97605559518d8bf9a02995698e613afbf5c8b
zhangfaliang <zhangfaliang@doublefs.com>
2023-10-23 16:55:21 +0800

    :construction: 清理图片

--- demo/src/components/GetMask/index.tsx
+++ demo/src/components/GetMask/index.tsx
@@ -4,16 +4,16 @@ import Button from "@mui/material/Button";
 import Switch from "@mui/material/Switch";
 import AddTaskIcon from "@mui/icons-material/AddTask";
 import PlaylistAddIcon from "@mui/icons-material/PlaylistAdd";
-import RefreshIcon from '@mui/icons-material/Refresh';
-import CloseIcon from '@mui/icons-material/Close';
-import HelpIcon from '@mui/icons-material/Help';
+import RefreshIcon from "@mui/icons-material/Refresh";
+import CloseIcon from "@mui/icons-material/Close";
+import HelpIcon from "@mui/icons-material/Help";
 import { useNavigate } from "react-router-dom";
 import CloudUploadIcon from "@mui/icons-material/CloudUpload";
 import ListAltIcon from "@mui/icons-material/ListAlt";
 import { get } from "lodash";
 import TextSnippetIcon from "@mui/icons-material/TextSnippet";
 import Typography from "@mui/material/Typography";
-import Popover from '@mui/material/Popover';
+import Popover from "@mui/material/Popover";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import JSZip from "jszip";
@@ -21,8 +21,8 @@ import "./index.scss";
 
 const CropImg = ({ handleMouseMove, uploadURL = "/save_image" }: any) => {
   const imgRef = useRef<HTMLImageElement>(null);
-  const isControlKey = useRef(false)
-  const [isDelete, setIsDelete] = useState(false)
+  const isControlKey = useRef(false);
+  const [isDelete, setIsDelete] = useState(false);
   const [aspect, setAspect] = useState<number | undefined>(16 / 9);
   const [downImg, setDownImg] = useState("");
 
@@ -33,7 +33,7 @@ const CropImg = ({ handleMouseMove, uploadURL = "/save_image" }: any) => {
     processImgType: [processImgType, setProcessImgType],
     maskImgList: [maskImgList, setMaskImgList],
     showMaskImgList: [showMaskImgList, setShowMaskImgList],
-    rangeRects: [rangeRects, setRangeRects]
+    rangeRects: [rangeRects, setRangeRects],
   } = useContext(AppContext)!;
   const navigate = useNavigate();
 
@@ -45,13 +45,14 @@ const CropImg = ({ handleMouseMove, uploadURL = "/save_image" }: any) => {
 
   useEffect(() => {
     setAspect(undefined);
-    document.addEventListener('keydown', documentKeydown) 
-    document.addEventListener('keyup', documentKeyup)
+    document.addEventListener("keydown", documentKeydown);
+    document.addEventListener("keyup", documentKeyup);
     return () => {
-      document.removeEventListener('keydown', documentKeydown )
-      document.removeEventListener('keyup', documentKeyup)
-      setRangeRects([])
+      document.removeEventListener("keydown", documentKeydown);
+      document.removeEventListener("keyup", documentKeyup);
+      setRangeRects([]);
       setMaskImgList([]);
+      setImage(null);
     };
   }, []);
   let name = "";
@@ -102,72 +103,75 @@ const CropImg = ({ handleMouseMove, uploadURL = "/save_image" }: any) => {
     ]);
     setShowMaskImgList(true);
   };
-  let isMouseDown = useRef(false)
+  let isMouseDown = useRef(false);
   function getPosition(e: any) {
     if (isControlKey.current) {
-      isMouseDown.current = true
+      isMouseDown.current = true;
       if (isMouseDown.current) {
         let el = e.nativeEvent.target;
         const clientRect = el.getBoundingClientRect();
         let x = e.clientX - Math.ceil(clientRect.left);
         let y = e.clientY - Math.ceil(clientRect.top);
-        setRangeRects([...rangeRects, {x, y, w: 0, h: 0, id: new Date().getTime()}])
+        setRangeRects([
+          ...rangeRects,
+          { x, y, w: 0, h: 0, id: new Date().getTime() },
+        ]);
       }
     }
   }
-  let isSelected = useRef(false)
+  let isSelected = useRef(false);
   function getClientRect(e: any) {
-    if (!isMouseDown.current) return
-    isSelected.current = true
+    if (!isMouseDown.current) return;
+    isSelected.current = true;
     let el = e.nativeEvent.target;
     const clientRect = el.getBoundingClientRect();
     let currentX = e.clientX - Math.ceil(clientRect.left);
     let currentY = e.clientY - Math.ceil(clientRect.top);
-    const [rect] = rangeRects.slice(-1)
+    const [rect] = rangeRects.slice(-1);
     let w = currentX - rect.x;
     let h = currentY - rect.y;
-    rect.w = w
-    rect.h = h
-    rangeRects[rangeRects.length-1] = rect
-    setRangeRects([...rangeRects])
-    e.preventDefault()
+    rect.w = w;
+    rect.h = h;
+    rangeRects[rangeRects.length - 1] = rect;
+    setRangeRects([...rangeRects]);
+    e.preventDefault();
   }
   function onMouseUp() {
-    isMouseDown.current = false
+    isMouseDown.current = false;
     setTimeout(() => {
-      isSelected.current = false
+      isSelected.current = false;
     }, 50);
   }
   function rectMaskClick(i: number) {
-    rangeRects.splice(i, 1)
-    setRangeRects([...rangeRects])
+    rangeRects.splice(i, 1);
+    setRangeRects([...rangeRects]);
   }
-  function documentKeydown(e: {keyCode: number}) {
+  function documentKeydown(e: { keyCode: number }) {
     if (e.keyCode == 17) {
-      isControlKey.current = true
+      isControlKey.current = true;
     }
     if (e.keyCode == 18) {
-      setIsDelete(true)
+      setIsDelete(true);
     }
   }
-  function documentKeyup(e: {keyCode: number}) {
+  function documentKeyup(e: { keyCode: number }) {
     if (e.keyCode == 17) {
-      isControlKey.current = false
+      isControlKey.current = false;
     }
     if (e.keyCode == 18) {
-      setIsDelete(false)
+      setIsDelete(false);
     }
   }
-  
+
   function reset() {
-    const useImgWrapper:any = document.getElementById("useImgWrapper")
-    const maskPointers = useImgWrapper.querySelectorAll(".maskPointer")
+    const useImgWrapper: any = document.getElementById("useImgWrapper");
+    const maskPointers = useImgWrapper.querySelectorAll(".maskPointer");
     maskPointers.forEach((el: HTMLDivElement) => {
-      useImgWrapper.removeChild(el)
+      useImgWrapper.removeChild(el);
     });
-    setRangeRects([])
-    setPreviousMask(null)
-    setMaskImg(null)
+    setRangeRects([]);
+    setPreviousMask(null);
+    setMaskImg(null);
   }
 
   function downloadFolder({ maskSrc, maskName }: any) {
@@ -234,47 +238,67 @@ const CropImg = ({ handleMouseMove, uploadURL = "/save_image" }: any) => {
                 <RefreshIcon />
                 重置
               </Button>
-              <div style={{display: "flex", alignItems: "center", marginLeft: "12px"}}>
-                <span>设置隔离区域</span>  
+              <div
+                style={{
+                  display: "flex",
+                  alignItems: "center",
+                  marginLeft: "12px",
+                }}
+              >
+                <span>设置隔离区域</span>
                 <Typography
-                  aria-owns={open ? 'mouse-over-popover' : undefined}
+                  aria-owns={open ? "mouse-over-popover" : undefined}
                   aria-haspopup="true"
                   onMouseEnter={handlePopoverOpen}
                   onMouseLeave={handlePopoverClose}
                 >
-                  <HelpIcon color="disabled" style={{width: "18px"}}/>
+                  <HelpIcon color="disabled" style={{ width: "18px" }} />
                 </Typography>
-                <Popover id="mouse-over-popover"
+                <Popover
+                  id="mouse-over-popover"
                   sx={{
-                    pointerEvents: 'none',
+                    pointerEvents: "none",
                   }}
                   open={open}
                   anchorEl={anchorEl}
                   anchorOrigin={{
-                    vertical: 'bottom',
-                    horizontal: 'left',
+                    vertical: "bottom",
+                    horizontal: "left",
                   }}
                   transformOrigin={{
-                    vertical: 'top',
-                    horizontal: 'left',
+                    vertical: "top",
+                    horizontal: "left",
                   }}
                   onClose={handlePopoverClose}
-                  disableRestoreFocus>
-                  <Typography sx={{ p: 1 }} fontSize={14} color={"#666"} width={250}>
-                    <span style={{display: "flex"}}>
+                  disableRestoreFocus
+                >
+                  <Typography
+                    sx={{ p: 1 }}
+                    fontSize={14}
+                    color={"#666"}
+                    width={250}
+                  >
+                    <span style={{ display: "flex" }}>
                       <span>1、</span>
-                      <span>设置隔离区：打开按钮或按control键，按下鼠标并拖动，可选中隔离区域，将不想被连带选中的区域进行隔离。(设置完隔离区，需将按钮关闭，进行选中mask操作)</span>
+                      <span>
+                        设置隔离区：打开按钮或按control键，按下鼠标并拖动，可选中隔离区域，将不想被连带选中的区域进行隔离。(设置完隔离区，需将按钮关闭，进行选中mask操作)
+                      </span>
                     </span>
-                    <br/>
-                    <span style={{display: "flex"}}>
+                    <br />
+                    <span style={{ display: "flex" }}>
                       <span>2、</span>
-                      <span>取消隔离区：按住键盘 Alt/option 键，点击“关闭”按钮，可删除选中区域。</span>
+                      <span>
+                        取消隔离区：按住键盘 Alt/option
+                        键，点击“关闭”按钮，可删除选中区域。
+                      </span>
                     </span>
                   </Typography>
                 </Popover>
-                <Switch onChange={({target: {checked}}) => {
-                  isControlKey.current = checked
-                }}/>
+                <Switch
+                  onChange={({ target: { checked } }) => {
+                    isControlKey.current = checked;
+                  }}
+                />
               </div>
             </div>
             <div className="use_img_mask_wrapper">
@@ -284,8 +308,8 @@ const CropImg = ({ handleMouseMove, uploadURL = "/save_image" }: any) => {
                 onLoad={onImageLoad}
                 onClick={(e: any) => {
                   if (isControlKey.current || isSelected.current) {
-                    isSelected.current = false
-                    return
+                    isSelected.current = false;
+                    return;
                   }
                   processImgType === "mask" && handleMouseMove(e);
                 }}
@@ -296,14 +320,14 @@ const CropImg = ({ handleMouseMove, uploadURL = "/save_image" }: any) => {
                   handleMouseMove({ ...event, clickType: "right" });
                 }}
                 onMouseDown={(event) => {
-                  getPosition(event)
+                  getPosition(event);
                 }}
                 onMouseMove={(event) => {
-                  getClientRect(event)
+                  getClientRect(event);
                   event.preventDefault();
                 }}
                 onMouseUp={() => {
-                  onMouseUp()
+                  onMouseUp();
                 }}
                 src={image.src}
                 className={`target_img`}
@@ -316,29 +340,35 @@ const CropImg = ({ handleMouseMove, uploadURL = "/save_image" }: any) => {
                 ></img>
               )}
               {rangeRects.map((rect: any, i: number) => {
-                return <div className={`rect_mask`} key={rect.id}
-                  style={{
-                    left: rect.x,
-                    top: rect.y,
-                    width: rect.w + 'px',
-                    height: rect.h + 'px',
-                    display: rect.w > 5 ? 'block' : 'none',
-                    pointerEvents: isDelete ? 'inherit' : 'none'
-                  }}
-                  onContextMenu={(event) => {
-                    event.preventDefault();
-                  }}
-                >
-                  <span
-                    className={'close_button'}
+                return (
+                  <div
+                    className={`rect_mask`}
+                    key={rect.id}
                     style={{
-                      display: isDelete ? 'block' : 'none',
+                      left: rect.x,
+                      top: rect.y,
+                      width: rect.w + "px",
+                      height: rect.h + "px",
+                      display: rect.w > 5 ? "block" : "none",
+                      pointerEvents: isDelete ? "inherit" : "none",
                     }}
-                    onClick={() => {
-                      rectMaskClick(i)
+                    onContextMenu={(event) => {
+                      event.preventDefault();
                     }}
-                  ><CloseIcon /></span>
-                </div>
+                  >
+                    <span
+                      className={"close_button"}
+                      style={{
+                        display: isDelete ? "block" : "none",
+                      }}
+                      onClick={() => {
+                        rectMaskClick(i);
+                      }}
+                    >
+                      <CloseIcon />
+                    </span>
+                  </div>
+                );
               })}
             </div>
           </div>
